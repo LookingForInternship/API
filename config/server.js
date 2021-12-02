22ddf0bdec39b659e71e5b49678ac81fbca4e2d2
chaigqn <quentinchaignaudpro@gmail.com>
2021-12-02 22:05:46 +0100

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3a34b564686056bad1941a068bb2faca'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '064f3d35-bf5b-431c-a788-dd0c819d958c'
+};
